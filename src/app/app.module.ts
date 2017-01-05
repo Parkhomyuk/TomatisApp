0d7077d8b7c843eac4d324f6590d5df4753bde84
Alexey Parkhomyuk <parkhomyukalex@gmail.com>
2017-01-05 13:42:50 +0200

    main page ready

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+//noinspection TypeScriptCheckImport
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import {NavbarComponent} from "./components/navbar/navbar.component.ts";
+import {AboutComponent} from "./components/about/about.component";
+import {ServiComponent} from "./components/servi/servi.component";
+import {BlogComponent} from "./components/blog/blog.component";
+import {ContactComponent} from "./components/contacts/contact.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, NavbarComponent, AboutComponent, ServiComponent, BlogComponent, ContactComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/servi/servi.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'servi',
+  templateUrl: './servi.component.html',
+
+})
+export class ServiComponent {
+
+}
